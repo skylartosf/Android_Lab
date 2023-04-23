625461f443c2de8bd0e5767bb1c2bc1a32ca5f67
Skylar <twinkle991@naver.com>
2023-04-23 18:47:25 +0900

    [FEAT/145] using CountDownTimer

--- 7mWorkoutApp/app/src/main/java/com/example/a7mworkoutapp/ExerciseActivity.kt
+++ 7mWorkoutApp/app/src/main/java/com/example/a7mworkoutapp/ExerciseActivity.kt
@@ -2,10 +2,15 @@ package com.example.a7mworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7mworkoutapp.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,42 @@ class ExerciseActivity : AppCompatActivity() {
         binding.tbEx.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setRestPb()
+    }
+
+    private fun setupRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestPb()
+    }
+
+    private fun setRestPb() {
+        binding.pb.progress = restProgress
+        restTimer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding.pb.progress = 10 - restProgress
+                binding.tvTimer.text = (10 - restProgress).toString()
+            }
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        //binding = null
     }
 }
