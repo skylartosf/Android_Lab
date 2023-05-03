77f75f20f09ab7aee06c677604c369d72883f1c8
Skylar <olfaminion0116@naver.com>
2023-05-03 16:33:48 +0900

    [155] pass SampleData to Adapter on MainActivity

--- recycler-view/src/main/java/com/example/recycler_view/MainActivity.kt
+++ recycler-view/src/main/java/com/example/recycler_view/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.recycler_view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recycler_view.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val adapter = Adapter(SampleData.dataList)
+        with(binding) {
+            rv.adapter = adapter
+            //rv.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        }
     }
 }
