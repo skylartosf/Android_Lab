6c37f5a1ff68e5c92c108abd2986995f54244042
Skylar <olfaminion0116@naver.com>
2023-05-03 13:41:28 +0900

    [155/rv] create rv & item layout, a data model(class), temp data(object)

---
+++ TextToSpeech/viewbindingrv/src/main/java/com/example/viewbindingrv/Task.kt
@@ -0,0 +1,6 @@
+package com.example.viewbindingrv
+
+data class Task(
+    val title: String,
+    val time: String
+    )
---
+++ TextToSpeech/viewbindingrv/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,15 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Take a walk", "7:00 a.m."),
+        Task("Eat breakfast", "8:00 a.m."),
+        Task("Attend a meeting", "9:00 a.m."),
+        Task("Read a book", "12:00 p.m."),
+        Task("Eat lunch", "2:00 p.m."),
+        Task("Buy groceries", "4:00 p.m."),
+        Task("Watch Youtube videos", "6:00 a.m."),
+        Task("Eat dinner", "7:00 a.m."),
+        Task("Go to bed", "9:00 p.m."),
+    )
+}
