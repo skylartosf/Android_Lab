105310dcabbfaa9d0fa4984aa935dbba12bfa44b
Skylar <olfaminion0116@naver.com>
2023-05-03 16:26:24 +0900

    [155] create DataModel, SampleData(object)

---
+++ recycler-view/src/main/java/com/example/recycler_view/DataModel.kt
@@ -0,0 +1,6 @@
+package com.example.recycler_view
+
+data class DataModel(
+    val title: String,
+    val time: String
+)
---
+++ recycler-view/src/main/java/com/example/recycler_view/SampleData.kt
@@ -0,0 +1,19 @@
+package com.example.recycler_view
+
+object SampleData {
+    val dataList = arrayListOf<DataModel>(
+        DataModel("Wake up and take a shower", "6:00 a.m."),
+        DataModel("Take a walk", "7:00 a.m."),
+        DataModel("Eat breakfast", "8:00 a.m."),
+        DataModel("Attend a meeting", "9:00 a.m."),
+        DataModel("Read a book", "12:00 p.m."),
+        DataModel("Eat lunch", "2:00 p.m."),
+        DataModel("Buy groceries", "4:00 p.m."),
+        DataModel("Watch Youtube videos", "6:00 p.m."),
+        DataModel("Eat dinner", "7:00 p.m."),
+        DataModel("Go to bed", "9:00 p.m."),
+        DataModel("Flying to San Francisco", "10:00 p.m."),
+        DataModel("Meeting Kuris", "11:00 p.m."),
+        DataModel("Heading to rooftop party in NYC", "11:30 p.m."),
+    )
+}
